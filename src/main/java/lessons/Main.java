020925e251c1a92ab36a41fc3f4ebdd5cdefd11c
Evg Volygina <v.evg@yandex.ru>
2025-09-14 23:12:03 +0300

    ДЗ package lessons.lesson04.homework

---
+++ src/main/java/lessons/Main.java
@@ -0,0 +1,12 @@
+package lessons;
+import lessons.lesson5.lesson5Task;
+import lessons.lesson6.lesson6Task;
+
+import static java.sql.DriverManager.println;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    }
+    }
